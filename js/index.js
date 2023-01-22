7887b2f6c985c90804ea455d280ea94c0a3152b0
zsorosz <oroszi.zsofi@gmail.com>
2023-01-22 20:23:29 +0100

    Add keypress eventListeners and functions

--- js/index.js
+++ js/index.js
@@ -4,6 +4,7 @@ ctx.canvas.style = "border: 1px solid black";
 
 let gameOver = false;
 let animateId;
+let score = 0;
 
 const bgImg1 = new Image();
 bgImg1.src = "../images/room.png";
@@ -17,10 +18,6 @@ babyImg.src = "../images/baby.png";
 let babyY = 450;
 
 let itemX = 400;
-let randomItem;
-const createRandomItem = () => {
-  randomItem = items[Math.floor(Math.random() * 11)];
-};
 let randomItems = [];
 
 class Item {
@@ -33,24 +30,30 @@ class Item {
   draw() {
     let itemImg = new Image();
     itemImg.src = this.img;
-    this.xPos -= 2;
-    ctx.drawImage(itemImg, this.xPos, 400, 400, 400);
+    this.xPos -= 3;
+    ctx.drawImage(itemImg, this.xPos, 400, 500, 500);
   }
 }
 
 const animate = () => {
-  /////////Background animation//////////
   ctx.drawImage(bgImg1, bg1X, 0, canvas.width, canvas.height);
   ctx.drawImage(bgImg2, bg2X, 0, canvas.width, canvas.height);
   ctx.drawImage(babyImg, 100, babyY, 400, 400);
 
+  ctx.font = "48px serif";
+  ctx.fillText(`Score: ${score}`, 10, 48);
+
   randomItems.forEach((item) => {
     item.draw(item);
+    if (item.xPos < 200 && item.type === "baby") {
+      randomItems.shift();
+    } else if (item.xPos < 200 && item.type === "adult") {
+      gameOver = true;
+    }
   });
-  randomItems = randomItems.filter((item) => item.xPos > 0);
 
-  bg1X -= 2;
-  bg2X -= 2;
+  bg1X -= 3;
+  bg2X -= 3;
 
   if (bg1X < -canvas.width) {
     bg1X = canvas.width;
@@ -59,8 +62,9 @@ const animate = () => {
     bg2X = canvas.width;
   }
 
-  if (animateId % 200 === 0) {
-    createRandomItem();
+  if (animateId % 100 === 0) {
+    let randomItem = items[Math.floor(Math.random() * 11)];
+    console.log(randomItem);
     randomItems.push(new Item(randomItem.img, randomItem.type, canvas.width));
   }
 
@@ -84,3 +88,19 @@ window.onload = () => {
   //   };
   startGame();
 };
+
+document.addEventListener("keydown", (event) => {
+  if (event.key === "ArrowLeft" && randomItems[0].type === "baby") {
+    randomItems.shift();
+    score++;
+    console.log("left");
+  } else if (event.key === "ArrowLeft" && randomItems[0].type === "adult") {
+    gameOver = true;
+  }
+  if (event.key === "ArrowRight" && randomItems[0].type === "baby") {
+    randomItems.shift();
+  } else if (event.key === "ArrowRight" && randomItems[0].type === "adult") {
+    randomItems.shift();
+    score++;
+  }
+});
